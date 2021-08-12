f17b54b65017edb44500bbd6168146176c7f5554
Alvaro Tinoco <alv_tinoco_97@hotmail.com>
2021-08-12 09:00:04 +0200

    fix(header): proper sizing

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,11 +2,11 @@ import { h } from "preact";
 import { Link } from "preact-router/match";
 import Cart from "./components/cart";
 
-const Header = ({ cartItems = 0 }) => (
-  <header class="flex justify-between border-b bg-white pb-1">
+export const Header = ({ cartItems = 0 }) => (
+  <header class="flex justify-between border-b bg-white p-3 px-5 items-center">
     <Link href="/" class="flex">
       <img src={"/assets/icons/favicon-32x32.png"} />
-      <h1 class="text-black">Preact App</h1>
+      <h1 class="text-black text-2xl ml-2">Preact App</h1>
     </Link>
     <Cart number={cartItems} />
   </header>
