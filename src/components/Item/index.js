8bb390fdff42c2f09214d1d3454524ccda0751ea
Alvaro Tinoco <alv_tinoco_97@hotmail.com>
2021-08-21 19:30:14 +0200

    refactor(item): improve responsive design

--- src/components/Item/index.js
+++ src/components/Item/index.js
@@ -1,12 +1,18 @@
 import { h } from "preact";
 
 export const Item = ({ imgSrc, model, brand, price }) => (
-  <article class="text-sm font-bold uppercase text-justify">
-    <img src={imgSrc} />
-    <h2 class="text-base">{model}</h2>
-    <span class="text-gray-700">
-      {brand} <i class="text-gray-400">&bull;</i> {price} <small>EUR</small>
-    </span>
+  <article class="text-sm font-bold uppercase text-justify sm:w-max mx-auto flex sm:block w-9/12">
+    <img
+      src={imgSrc}
+      async
+      class="max-h-[170px] sm:max-w-[140px] sm:max-h-full"
+    />
+    <div class="pl-2 sm:p-0">
+      <h2 class="text-base text-left">{model}</h2>
+      <span class="text-gray-700 block sm:inline-block">
+        {brand} <i class="text-gray-400">&bull;</i> {price} <small>EUR</small>
+      </span>
+    </div>
   </article>
 );
 
