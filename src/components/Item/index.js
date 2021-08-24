394ada40a72d6d9370cc6e7f0186828a5016d5c6
Alvaro Tinoco <alv_tinoco_97@hotmail.com>
2021-08-24 23:33:02 +0200

    refactor(home): use image component

--- src/components/Item/index.js
+++ src/components/Item/index.js
@@ -1,10 +1,10 @@
 import { h } from "preact";
+import { Image } from "../Image";
 
 export const Item = ({ imgSrc, model, brand, price }) => (
   <article class="text-sm font-bold uppercase text-justify sm:w-max mx-auto flex sm:block w-9/12">
-    <img
+    <Image
       src={imgSrc}
-      async
       class="max-h-[170px] sm:max-w-[140px] sm:max-h-full min-h-[100px]"
     />
     <div class="pl-2 sm:p-0">
