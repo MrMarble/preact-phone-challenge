b106c6dbf72d84d8442aad763c558dd9f1cbe9d6
Alvaro Tinoco <alv_tinoco_97@hotmail.com>
2021-08-24 23:33:24 +0200

    feat(component): loader

---
+++ src/components/Loader/index.js
@@ -0,0 +1,10 @@
+export const Loader = () => (
+  <section>
+    <img
+      src="/assets/svg/grid.svg"
+      class="mx-auto absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2"
+    />
+  </section>
+);
+
+export default Loader;
