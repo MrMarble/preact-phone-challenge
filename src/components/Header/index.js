48476ce2b641b723af50e4ee3a38eedd01b67b43
Alvaro Tinoco <alv_tinoco_97@hotmail.com>
2021-08-24 21:24:39 +0200

    feat(header): responsive logo

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -8,7 +8,7 @@ export const Header = ({ cartItems = 0 }) => {
     <header class="flex justify-between border-b bg-white p-3 px-5 items-center">
       <Link href="/" class="flex">
         <img src={"/assets/icons/favicon-32x32.png"} />
-        <h1 class="text-black text-2xl ml-2">Preact App</h1>
+        <h1 class="text-black text-2xl ml-2 hidden sm:block">Preact App</h1>
       </Link>
       <Breadcrumbs />
       <Cart number={cartItems} />
